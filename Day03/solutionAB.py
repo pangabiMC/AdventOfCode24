38c8c021d32b81b4d7088a58102720c98573119d
pangabiMC <gab.monus@gmail.com>
2024-12-03 09:43:18 +0000

    Day 3 Done

--- Day03/solutionAB.py
+++ Day03/solutionAB.py
@@ -12,3 +12,15 @@ for m in re.findall(regex, data):
 print(result)
 
 # part 2
+regex = "(don't)|(do)|(mul\((\d{1,3}),(\d{1,3})\))"
+result = 0
+do = True
+for m in re.findall(regex, data):
+    if m[3] and m[4] and do: #it's a mul group
+        result += int(m[3]) * int(m[4])
+    elif m[1]: # "do"
+        do = True
+    elif m[0]: # "don't":
+        do = False
+
+print(result)

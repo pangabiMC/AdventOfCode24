cca32d0c97c9bb96dea044f81c25668561c7328c
pangabiMC <gab.monus@gmail.com>
2024-12-10 19:05:12 +0000

    day 8 comments

--- Day08/solutionAB.py
+++ Day08/solutionAB.py
@@ -9,19 +9,21 @@ with open(filename) as file:
 length = len(matr[0])
 
 # part 1
+
 antinodes = []
-frequencies = set(matr.flatten())
+frequencies = set(matr.flatten()) # each character other than . is a separate frequency
 frequencies.remove('.')
 for f in frequencies:
-    antennas = np.array([*zip(*np.where(matr == f))])
-    for (a1, a2) in combinations(antennas, 2):
+    antennas = np.array([*zip(*np.where(matr == f))]) # only looking at the antennas for this frequency
+    for (a1, a2) in combinations(antennas, 2): # gets all possible pairs within the array
         antinodes.append(a1 - (a2 - a1))
         antinodes.append(a2 + (a2 - a1))
-antinodes = np.unique(antinodes, axis=0)
-result = sum(1 for a in antinodes if a[0] in range(0, length) and a[1] in range(0, length))
+antinodes = np.unique(antinodes, axis=0) # remove the overlapping antinodes
+result = sum(1 for a in antinodes if a[0] in range(0, length) and a[1] in range(0, length)) # count those that are within the map
 print(result)
 
 # part 2
+# same as part 1 but we add the difference vector as many times as possible - this will also cover the antennas as they should
 antinodes = []
 for f in frequencies:
     antennas = np.array([*zip(*np.where(matr == f))])

54c1bb84459bf9ba8e8540a0fa01616584d0654c
pangabiMC <gab.monus@gmail.com>
2025-01-04 13:00:37 +0000

    code cleanup and comments

--- Day23/Part12.py
+++ Day23/Part12.py
@@ -3,100 +3,38 @@ filename = "Day23/inputtest" if isTest else "Day23/input"
 
 # init data
 with open(filename) as file:
-    data = [line.strip() for line in file]
+    Data = [line.strip() for line in file]
 
-connections = dict()
-tmachines = set()
-for d in data:
+Connections = dict()
+for d in Data:
     (c1, c2) = d.split('-') 
-    connections.setdefault(c1, set()).add(c2)
-    connections.setdefault(c2, set()).add(c1)
-    if c1[0] == 't':
-        tmachines.add(c1)
-    if c2[0] == 't':
-        tmachines.add(c2)
+    Connections.setdefault(c1, set()).add(c2)
+    Connections.setdefault(c2, set()).add(c1)
 
-nodes = list(connections)
-# adj = [[0] * len(nodes) for _ in range(len(nodes))]
-# for i in range(len(nodes)):
-#     for c in connections[nodes[i]]:
-#         adj[i][nodes.index(c)] = 1
-
-# def printAdjMatr(adj, nodes):
-#     print('   ' + ' '.join(nodes))
-#     for n in nodes:
-#         print(f"{n} {'  '.join(list(map(str, adj[nodes.index(n)])))}")
-
-# print('\r\n\r\n')
-# printAdjMatr(adj, nodes)
-
-# adj3 = np.linalg.matrix_power(adj, 3)
-# tlist = [n for n in nodes if n in tmachines]
-
-# result = 0
-# for i in range(len(tlist)):
-#     t = tlist[i]
-#     print(f'{t} - {adj3[nodes.index(t)][nodes.index(t)]}')
-#     result += adj3[nodes.index(t)][nodes.index(t)] // 2 - sum(adj[nodes.index(t)][nodes.index(t0)] for t0 in tlist[i+1:])
-
-# print(result)
-
-# def getNthNeighbour(node: str, n: int) -> set:
-#     if n == 1:
-#         return set(connections[node])
-#     s = set()
-#     for nn in connections[node]:
-#         s |= getNthNeighbour(nn, n - 1)
-#     return s
-
-# print(getNthNeighbour('td', 3))
-
-# def search(current: str, visited: set, depth: int):
-#     for n in connections[current]:
-#         if n not in visited:
-#             getNthNeighbour(n, 2)
-
-# def bfs(node:str, maxDepth = 3):
-#     q = deque([node])
-#     paths = {node : []}
-#     while q:
-#         curr = q.popleft()
-#         if len(paths[curr]) == maxDepth:
-#             break
-#         for n in connections[curr]:
-#             if n not in paths:
-#                 paths[n] = paths[curr] + [n]
-#                 q.append(n)
-#     return paths
-
-# print(bfs('td'))
-
-def generate_triangles(nodes):
+# Part1
+# Naive brute force, fast enough
+def generate_triangles(connections):
     visited = set()
-    for curr in nodes:
+    for curr in connections:
         loop_visited = set()
-        for next in nodes[curr]:
+        for next in connections[curr]:
             if next in visited:
                 continue
-            for nextnext in nodes[next]:
+            for nextnext in connections[next]:
                 if nextnext in visited or nextnext in loop_visited:
                     continue
-                if curr in nodes[nextnext]:
+                if curr in connections[nextnext]:
                     yield(curr, next, nextnext)
             loop_visited.add(next)
         visited.add(curr)
 
-result = 0        
-for t in generate_triangles(connections):
-    if any(ti[0] == 't' for ti in t):
-        result += 1
+result = sum(1 for t in generate_triangles(Connections) if any(ti[0] == 't' for ti in t))
 print(result)
 
-
-
-
+# Part2
+# using the Bron–Kerbosch algorithm
+# https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
 def bors_kerbosch_v1(R, P, X, G, C):
-
     if len(P) == 0 and len(X) == 0:
         if len(R) > 2:
             C.append(sorted(R))
@@ -107,9 +45,7 @@ def bors_kerbosch_v1(R, P, X, G, C):
         P.remove(v)
         X.add(v)
 
-C1 = []
-
-bors_kerbosch_v1(set([]), set(connections.keys()), set([]), connections, C1)
-
-c = max(sorted(C1), key=len)
+C = []
+bors_kerbosch_v1(set([]), set(Connections.keys()), set([]), Connections, C)
+c = max(sorted(C), key=len)
 print(','.join(c))
--- Day24/Part12.py
+++ Day24/Part12.py
@@ -13,7 +13,7 @@ class Var:
     value: int
 
 @dataclass
-class Op:
+class Gate:
     input1: Var
     input2: Var
     output: Var
@@ -42,7 +42,7 @@ class Op:
                 vars[self.output.token] = self.output.value
         return self.IsSet()
 
-Ops = []
+Gates = []
 Vars = {}
 for l in data:
     if len(l) > 4:
@@ -51,14 +51,17 @@ for l in data:
             Vars[m[0][0]] = int(m[0][1])
         else:
             m = re.findall("(\w{3})\s*(OR|AND|XOR)\s*(\w{3})\s*->\s*(\w{3})", l)
-            Ops.append(Op(input1=Var(m[0][0], None), input2=Var(m[0][2], None), output=Var(m[0][3], None), operand=m[0][1]))
+            Gates.append(Gate(input1=Var(m[0][0], None), input2=Var(m[0][2], None), output=Var(m[0][3], None), operand=m[0][1]))
 
-Ops2 = Ops.copy() # for Part2
+Gates_copy = Gates.copy() # for Part2
 
 # Part 1
-while len(Ops) > 0:
-    Ops = [o for o in Ops if not o.TrySet(Vars)]
+# Simple enough once the data is mapped to the Gate class with its functions
+# Just keep on resolving the gates until all is set
+while len(Gates) > 0:
+    Gates = [o for o in Gates if not o.TrySet(Vars)]
 
+# Read the output from vars beginning with z
 zvars = [v for v in Vars if v[0] == 'z']
 zvars.sort(reverse=True)
 result = 0
@@ -69,36 +72,39 @@ for z in zvars:
 print(result // 2)
 
 # Part 2
-# https://www.reddit.com/r/adventofcode/comments/1hla5ql/2024_day_24_part_2_a_guide_on_the_idea_behind_the/
+# Took a hint for this one, I didn't know what a ripple-carry adder was... 
+# https://en.wikipedia.org/wiki/Adder_(electronics)#Ripple-carry_adder
+# Some observation based on the schematic there (taken from reddit):
 #   If the output of a gate is z, then the operation has to be XOR unless it is the last bit.
 #   If the output of a gate is not z and the inputs are not x, y then it has to be AND / OR, but not XOR.
-
 #   If you have a XOR gate with inputs x, y, there must be another XOR gate with this gate as an input. 
-#       Search through all gates for an XOR-gate with this gate as an input; if it does not exist, your (original) XOR gate is faulty.
-#   Similarly, if you have an AND-gate, there must be an OR-gate with this gate as an input. If that gate doesn't exist, the original AND gate is faulty.
-#   (These don't apply for the gates with input x00, y00). 
+#   If you have an AND-gate, there must be an OR-gate with this gate as an input. If that gate doesn't exist, the original AND gate is faulty.
+#   (These two don't apply for the gates with input x00, y00). 
 
-def isFaulty(op: Op) -> bool:
+Gates = Gates_copy
+
+# Checks if the given gate is faulty
+def isFaulty(g: Gate) -> bool:
     #   If the output of a gate is z, then the operation has to be XOR unless it is the last bit.
-    if op.output.token[0] == 'z' and op.output.token != zvars[0] and op.operand != 'XOR':
+    if g.output.token[0] == 'z' and g.output.token != zvars[0] and g.operand != 'XOR':
         return True
     
     #   If the output of a gate is not z and the inputs are not x, y then it has to be AND / OR, but not XOR.
-    if op.output.token[0] != 'z' and op.input1.token[0] not in ['x', 'y'] and op.input2.token[0] not in ['x', 'y'] and op.operand == 'XOR':
+    if g.output.token[0] != 'z' and g.input1.token[0] not in ['x', 'y'] and g.input2.token[0] not in ['x', 'y'] and g.operand == 'XOR':
         return True
     
     #   If you have a XOR gate with inputs x, y, there must be another XOR gate with this gate as an input.
-    if op.operand == 'XOR' and op.input1.token[0] in ['x', 'y'] and op.input2.token[0] in ['x', 'y'] and op.input1.token not in ['x00', 'y00'] and op.input2.token not in ['x00', 'y00']:
-        if not any(o != op and o.operand == 'XOR' and (o.input1.token == op.output.token or o.input2.token == op.output.token) for o in Ops2):
+    if g.operand == 'XOR' and g.input1.token[0] in ['x', 'y'] and g.input2.token[0] in ['x', 'y'] and g.input1.token not in ['x00', 'y00'] and g.input2.token not in ['x00', 'y00']:
+        if not any(g_next != g and g_next.operand == 'XOR' and (g_next.input1.token == g.output.token or g_next.input2.token == g.output.token) for g_next in Gates):
             return True
     
     #   If you have an AND-gate, there must be an OR-gate with this gate as an input.
-    if op.operand == 'AND' and op.input1.token[0] in ['x', 'y'] and op.input2.token[0] in ['x', 'y'] and op.input1.token not in ['x00', 'y00'] and op.input2.token not in ['x00', 'y00']:
-        if not any(o != op and o.operand == 'OR' and (o.input1.token == op.output.token or o.input2.token == op.output.token) for o in Ops2):
+    if g.operand == 'AND' and g.input1.token[0] in ['x', 'y'] and g.input2.token[0] in ['x', 'y'] and g.input1.token not in ['x00', 'y00'] and g.input2.token not in ['x00', 'y00']:
+        if not any(g_next != g and g_next.operand == 'OR' and (g_next.input1.token == g.output.token or g_next.input2.token == g.output.token) for g_next in Gates):
             return True
     return False
 
-faulty = [o.output.token for o in Ops2 if isFaulty(o)]
+faulty = [g.output.token for g in Gates if isFaulty(g)]
 faulty.sort()
 print(','.join(faulty))
 
